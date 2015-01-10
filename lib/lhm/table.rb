32fa279d302987826484e802b8d66a38f7cc1e0d
Hannes Tydén <hannes@tyden.name>
2015-01-10 14:49:50 +0100

    [WiP] Add name suggestions

--- lib/lhm/table.rb
+++ lib/lhm/table.rb
@@ -15,6 +15,8 @@ module Lhm
       @ddl     = ddl
     end
 
+  # def satifies_monotonically_increasing_numeric_id_column_requirement?
+  # def has_monotonically_increasing_numeric_id_column?
     def satisfies_primary_key?
       !!((id = columns['id']) &&
         id[:type] =~ /int\(\d+\)/ &&
